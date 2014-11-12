66e99ee34b902be9c3b408ef5348ffccc3654f46
Stanley Yuan <stanley.yuan25@gmail.com>
2014-11-11 23:28:47 -0500

    Updated Level and Campaign

--- src/vooga/engine/gameRepresentation/Campaign.java
+++ src/vooga/engine/gameRepresentation/Campaign.java
@@ -1,5 +1,14 @@
 package vooga.engine.gameRepresentation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campaign {
+    
+    private List<Level> levels;
+    
+    public Campaign(){
+        levels = new ArrayList<Level>();
+    }
 
 }
