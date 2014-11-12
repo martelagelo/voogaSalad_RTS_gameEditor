61e95d2812ce6998baa4f1b1b18e61bd7cc28a18
regrenos <sk300@duke.edu>
2014-11-11 22:36:18 -0500

    conditions and actions are abstract

--- src/vooga/engine/gameRepresentation/actions/Action.java
+++ src/vooga/engine/gameRepresentation/actions/Action.java
@@ -4,12 +4,6 @@ import java.util.function.BiConsumer;
 import vooga.engine.gameRepresentation.GameElement;
 
 
-public class Action implements BiConsumer<GameElement, GameElement> {
-
-    @Override
-    public void accept (GameElement arg0, GameElement arg1) {
-        // TODO Auto-generated method stub
-
-    }
+public abstract class Action implements BiConsumer<GameElement, GameElement> {
 
 }
--- src/vooga/engine/gameRepresentation/conditions/Condition.java
+++ src/vooga/engine/gameRepresentation/conditions/Condition.java
@@ -4,12 +4,6 @@ import java.util.function.BiFunction;
 import vooga.engine.gameRepresentation.GameElement;
 
 
-public class Condition implements BiFunction<GameElement, GameElement, Boolean> {
-
-    @Override
-    public Boolean apply (GameElement arg0, GameElement arg1) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+public abstract class Condition implements BiFunction<GameElement, GameElement, Boolean> {
 
 }
