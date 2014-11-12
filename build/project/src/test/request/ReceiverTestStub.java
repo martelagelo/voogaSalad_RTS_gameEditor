37dbd776e561d81923c9016d13a25e79a4e3ba28
Joshua Miller <jmm146@duke.edu>
2014-11-12 14:13:50 -0500

    fl

---
+++ build/project/src/test/request/ReceiverTestStub.java
@@ -0,0 +1,54 @@
+package test.request;
+
+import java.util.Map;
+import vooga.request.IRequest;
+import vooga.request.Request;
+import vooga.request.forwarder.DeliveryException;
+import vooga.request.forwarder.IForwarder;
+import vooga.request.forwarder.InvalidAddressException;
+import vooga.request.forwarder.ReceiverNotFoundException;
+import vooga.request.receiver.IReceiver;
+import vooga.request.receiver.UnknownSenderException;
+
+/**
+ * Stub testing class for testing the receiver functionality
+ */
+public class ReceiverTestStub implements IReceiver{
+
+    private IRequest myLastRequest;
+    private IForwarder myMediator;
+    private int myPingPongCountMax;
+    private static final String pingPongLabel = "pingPong";
+
+    public ReceiverTestStub (IForwarder mediator, int pingPongMax) {
+        myMediator = mediator;
+        myPingPongCountMax = pingPongMax;
+    }
+
+    /**
+     * Receive here will pass back a new request to the sender of the original message
+     * as long as the number of "pings"/"pongs" hasn't exceeded a certain value
+     */
+    @Override
+    public void receive (IRequest request) throws UnknownSenderException {
+        myLastRequest = request;
+        if (request.message().containsKey(pingPongLabel)){
+            int curPingPongIteration = Integer.parseInt(request.message().get(pingPongLabel));
+            if (curPingPongIteration < myPingPongCountMax) {
+                Map<String, String> message = request.message();
+                message.put(pingPongLabel, Integer.toString(curPingPongIteration+1));
+                IRequest newRequest = new Request(request.receiver(), request.sender(), message);
+                try {
+                    myMediator.forward(newRequest);
+                }
+                catch (InvalidAddressException | ReceiverNotFoundException | DeliveryException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public IRequest lastRequest(){
+        return myLastRequest;
+    }
+}
---
+++ build/project/src/vooga/request/forwarder/AddressConflictException.java
@@ -0,0 +1,43 @@
+package vooga.request.forwarder;
+
+import vooga.request.receiver.IReceiver;
+
+/**
+ * An exception capturing when an address conflicts with a pre-existing address
+ */
+public class AddressConflictException extends Exception {
+
+    private static final long serialVersionUID = -3572949740426821224L;
+    private String myConflictingAddress;
+    private IReceiver myConflictingReceiver;
+
+    /**
+     * Create a new address conflict exception
+     * 
+     * @param conflictingAddress The address that conflicts with a pre-existing address
+     * @param conflictingReceiver The receiver that already possesses that address
+     */
+    public AddressConflictException(String conflictingAddress, IReceiver conflictingReceiver){
+        super(conflictingAddress + " is already in use by " + conflictingReceiver);
+        myConflictingAddress = conflictingAddress;
+        myConflictingReceiver = conflictingReceiver;
+    }
+
+    /**
+     * The conflicting address
+     * 
+     * @return The address that conflicts with a pre-existing address
+     */
+    public String conflictingAddress(){
+        return myConflictingAddress;
+    }
+
+    /**
+     * The conflicting receiver
+     * 
+     * @return The receiver that already possesses that address
+     */
+    public IReceiver conflictingReceiver(){
+        return myConflictingReceiver;
+    }
+}
