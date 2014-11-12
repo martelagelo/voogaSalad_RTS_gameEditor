3265dfc67b736388a89d7b0fe7a9119fe69bbfe2
regrenos <sk300@duke.edu>
2014-11-11 22:34:24 -0500

    actions are now consumers

--- src/vooga/engine/gameRepresentation/actions/Action.java
+++ src/vooga/engine/gameRepresentation/actions/Action.java
@@ -1,15 +1,15 @@
 package vooga.engine.gameRepresentation.actions;
 
-import java.util.function.BiFunction;
+import java.util.function.BiConsumer;
 import vooga.engine.gameRepresentation.GameElement;
 
 
-public class Action implements BiFunction<GameElement, GameElement, Boolean> {
+public class Action implements BiConsumer<GameElement, GameElement> {
 
     @Override
-    public Boolean apply (GameElement arg0, GameElement arg1) {
+    public void accept (GameElement arg0, GameElement arg1) {
         // TODO Auto-generated method stub
-        return null;
+
     }
 
 }
