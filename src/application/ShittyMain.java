dba90da7b495316354623a40d0a09d2b49b0c806
zdb3 <bears.zachary@gmail.com>
2014-11-13 18:58:12 -0500

    added shittyMain

---
+++ src/application/ShittyMain.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class ShittyMain extends Application {
+    @Override
+    public void start (Stage primaryStage) {
+        Group g = new Group();
+        Scene scene = new Scene(g, 600, 600);
+        primaryStage.setScene(scene);
+        shittyRun(g);
+        primaryStage.show();
+    }
+
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    public void shittyRun (Group g) {
+        System.out.println("Shitty running");
+
+    }
+
+}
