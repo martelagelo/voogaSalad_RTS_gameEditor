9dd4314f5ee5fc35cebfdc6a998d81ce7b047677
Joshua Miller <jmm146@duke.edu>
2014-11-13 23:44:28 -0500

    initial commit of working webserver

---
+++ web_serve/public/build/project/src/game_engine/Level.java
@@ -0,0 +1,16 @@
+package game_engine;
+
+/**
+ * 
+ * @author Joshua Miller
+ *
+ * A Game class which contains all the necessary
+ * information to build a single level in a game
+ */
+public class Level extends Describable {
+	
+	public Level (String name, String desc) {
+		super(name, desc);
+	}
+
+}
