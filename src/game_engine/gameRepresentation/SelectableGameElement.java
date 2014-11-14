a98936b80e51f47808cccfddb344a1397b780b50
regrenos <sk300@duke.edu>
2014-11-13 19:15:57 -0500

    adding some shitty things

--- src/game_engine/gameRepresentation/SelectableGameElement.java
+++ src/game_engine/gameRepresentation/SelectableGameElement.java
@@ -6,7 +6,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 
 
-public class SelectableGameElement extends DrawableGameElement {
+public class SelectableGameElement extends DrawableGameElement implements Sighted {
 
     protected GridPane abilityRepresentation;
     protected Node informationRepresentation;
