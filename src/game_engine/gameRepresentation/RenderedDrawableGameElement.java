c1971784409a4d56e69e4e46f40ce91e73a2f5de
regrenos <sk300@duke.edu>
2014-11-15 12:12:59 -0500

    unclear

--- src/game_engine/gameRepresentation/RenderedDrawableGameElement.java
+++ src/game_engine/gameRepresentation/RenderedDrawableGameElement.java
@@ -10,6 +10,6 @@ public class RenderedDrawableGameElement {
     private AnimationEngine animation;
 
     public RenderedDrawableGameElement (DrawableGameElement element) {
-
+        //TODO: use input to set state
     }
 }
