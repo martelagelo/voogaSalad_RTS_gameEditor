5d5fe62d4b9f7dea36d484fbd7b5137c8a516a9a
Rahul Harikrishnan <rh151@duke.edu>
2014-11-17 03:52:47 -0500

    Load Save working with tests and changes to adhere to API

--- src/util/ILoadSave.java
+++ src/util/ILoadSave.java
@@ -13,29 +13,31 @@ public interface ILoadSave {
 
     /**
      * Load a resource from a given file and return Java object representation
-     * @param filename
-     * @return Java class object
+     * 
+     * @param className
+     * @param filePath
+     * @return
      */
-    public <T> T loadResource (String filename);
+    public <T> T loadResource (Class className, String filePath);
 
     /**
      * Save a JSONable object to a library file
      * 
      * @param object
      *            that can be converted to JSON format
-     * @param fileName
+     * @param filePath
      *            to which object should be saved
      * @throws IOException
      *             in case of trouble reading
      */
-    public void save (JSONable object, String fileName) throws IOException;
+    public void save (JSONable object, String filePath) throws IOException;
 
     /**
      * 
      * @param image
-     * @param fileName
+     * @param filePath
      */
-    public void saveImage (Image image, String fileName);
+    public void saveImage (Image image, String filePath);
 
     /**
      * 
--- src/util/LoadSaveUtility.java
+++ src/util/LoadSaveUtility.java
@@ -20,45 +20,50 @@ import com.google.gson.JsonSyntaxException;
 
 /**
  * Utility class that handles the loading/saving of files.
+ * 
  * @author Rahul
  *
  */
 public class LoadSaveUtility implements ILoadSave {
+    public static String FILE_SEPARATOR = File.separator;
 
-    public  <T> T loadResource (String filename) {
+    public <T> T loadResource (Class className, String filePath) {
         Gson gson = new Gson();
 
         T jsonRepresentation = null;
         try {
-            jsonRepresentation =  (T) gson.fromJson(new FileReader(new File("src/resources/game/" + filename)), TestCampaign.class);
+            System.out.println(FILE_SEPARATOR);
+            jsonRepresentation = (T) gson.fromJson(new FileReader(new File("src" + FILE_SEPARATOR
+                    + "resources" + FILE_SEPARATOR + "game" + FILE_SEPARATOR + filePath)),
+                    className.class);
         } catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
             // TODO Need to communicate exception to calling class
         }
-        
+
         return jsonRepresentation;
     }
 
-    public void save (JSONable object, String filename) throws IOException {
+    public void save (JSONable object, String filePath) throws IOException {
         String json = object.toJSON();
-        FileWriter writer = new FileWriter("src/resources/game/" + filename);
+        FileWriter writer = new FileWriter("src" + FILE_SEPARATOR + "resources" + FILE_SEPARATOR
+                + "game" + FILE_SEPARATOR + filePath);
         writer.write(json);
         writer.close();
-    }  
+    }
 
-    public void saveImage(Image image, String filename) {
-        File output = new File("src/resources/img/" + filename);
+    public void saveImage (Image image, String filePath) {
+        File output = new File("src" + FILE_SEPARATOR + "resources" + FILE_SEPARATOR + "img"
+                + FILE_SEPARATOR + filePath);
         try {
             boolean b = ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", output);
             System.out.println(b);
-            
+
         } catch (Exception s) {
         }
     }
-    
 
     public Image loadImage (String filePath) {
-        Image image = new Image(getClass().getResourceAsStream("src/resources/img/" + filePath));
-        return image;    
+        Image image = new Image(getClass().getResourceAsStream("/resources/img/" + filePath));
+        return image;
     }
-
 }
