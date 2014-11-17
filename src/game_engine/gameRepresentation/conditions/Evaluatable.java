89d63468fa08a7984a7570a2827cdea8de288541
zdb3 <bears.zachary@gmail.com>
2014-11-17 18:48:31 -0500

    Refining regexes

--- src/game_engine/gameRepresentation/conditions/Evaluatable.java
+++ src/game_engine/gameRepresentation/conditions/Evaluatable.java
@@ -1,21 +1,18 @@
 package game_engine.gameRepresentation.conditions;
 
-import game_engine.gameRepresentation.stateRepresentation.gameElement.GameElementState;
-
-
 /**
  * An object that can evaluate a condition on an element to return a boolean value
- * 
+ *
  * @author Zach
  *
  */
 public interface Evaluatable {
     /**
      * Evaluate and return a boolean of the result
-     * 
+     *
      * @param element an optional element for the Evaluatable to evaluate on.
      */
-    //TODO implement the idea of null elements to allow this to evaluate on standard elements
-    public boolean evaluate(ElementPair elements);
+    // TODO implement the idea of null elements to allow this to evaluate on standard elements
+    public boolean evaluate (ElementPair elements);
 
 }
