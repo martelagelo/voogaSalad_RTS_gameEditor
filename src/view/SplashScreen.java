3583722c7dfca1013a420e6dc208a3379fefbdf4
Joshua Miller <jmm146@duke.edu>
2014-11-18 20:05:27 -0500

    search im myGames folder for games and add them to list

--- src/view/SplashScreen.java
+++ src/view/SplashScreen.java
@@ -1,11 +1,26 @@
 package view;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Observable;
+import java.util.stream.Collectors;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
 
+/**
+ * 
+ * @author Jonathan
+ * @author Nishad
+ * @author Joshua
+ *
+ */
 
 
 public class SplashScreen extends GUIScene implements GUIController {
@@ -13,6 +28,7 @@ public class SplashScreen extends GUIScene implements GUIController {
     @FXML private GridPane splash;
     @FXML private Button launchEditorButton;
     @FXML private Button launchRunnerButton;
+    @FXML private ComboBox myGames;
     private static final String LAUNCH_EDITOR = "Launch Editor";
     private static final String LAUNCH_RUNNER = "Launch Runner";
 
@@ -21,6 +37,15 @@ public class SplashScreen extends GUIScene implements GUIController {
     public void initialize () {
         launchEditorButton.setText(LAUNCH_EDITOR);
         launchRunnerButton.setText(LAUNCH_RUNNER);
+        
+        File folder = new File("./myGames/");
+        List<File> files = Arrays.asList(folder.listFiles());
+        List<String> gameNames = files
+        		.stream()
+        		.filter(f -> f.isDirectory())
+        		.map(f -> f.getName())
+        		.collect(Collectors.toList());   
+        myGames.setItems(FXCollections.observableArrayList(gameNames));
         
         launchEditorButton.setOnAction(e -> switchScene(ViewScreen.EDITOR, ""));
         //TODO Change to ViewScreen.RUNNER, also fill in String game (2nd argument)
