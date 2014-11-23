1e41b4f4141576f4833b58d261266649c864b59f
Michael Deng <mdeng123@gmail.com>
2014-11-23 16:12:26 -0500

    reformatted animationPlayer, DrawableGameElement, and SelectableGameElement

--- src/game_engine/gameRepresentation/renderedRepresentation/DrawableGameElement.java
+++ src/game_engine/gameRepresentation/renderedRepresentation/DrawableGameElement.java
@@ -14,11 +14,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
-
 /**
- * A game element that is capable of being drawn. Combines a game element state as well as an
- * animation player. Essentially wraps a savable game element state with a drawable wrapper to avoid
- * JavaFX serialization problems.
+ * A game element that is capable of being drawn. Combines a game element state
+ * as well as an animation player. Essentially wraps a savable game element
+ * state with a drawable wrapper to avoid JavaFX serialization problems.
  * 
  * @author Zach, Jonathan, Nishad, Rahul, John L.
  *
@@ -26,116 +25,121 @@ import javafx.scene.layout.VBox;
 public class DrawableGameElement extends GameElement implements Displayable {
 
 	final static String SELECT_ARROW_URL = "resources/img/Red_Arrow_Down.png";
-    private DrawableGameElementState myState;
-    protected AnimationPlayer myAnimation;
-    protected Group myDisplay;
-    protected VBox myDisplayVBox;
-    private Image mySelectedImage;
+	private DrawableGameElementState myState;
+	protected AnimationPlayer myAnimation;
+	protected Group myDisplay;
+	protected VBox myDisplayVBox;
+	private Image mySelectedImage;
 	private ImageView mySelectedImageView;
 
-    /**
-     * Create a drawable game element from the given state
-     * 
-     * @param element the state of the drawable element
-     */
-    public DrawableGameElement (DrawableGameElementState element) {
-        super(element);
-        myState = element;
-        Spritesheet spritesheet = element.getSpritesheet();
-        myAnimation =
-                new AnimationPlayer(new Image(spritesheet.imageTag), spritesheet.frameDimensions,
-                                    spritesheet.numCols);
-        myDisplay = new Group();
-        myDisplayVBox = new VBox(1);
-        mySelectedImage = new Image(SELECT_ARROW_URL);
+	/**
+	 * Create a drawable game element from the given state
+	 * 
+	 * @param element
+	 *            the state of the drawable element
+	 */
+	public DrawableGameElement(DrawableGameElementState element) {
+		super(element);
+		myState = element;
+		Spritesheet spritesheet = element.getSpritesheet();
+		myAnimation = new AnimationPlayer(new Image(spritesheet.imageTag),
+				spritesheet.frameDimensions, spritesheet.numCols);
+		myDisplay = new Group();
+		myDisplayVBox = new VBox(1);
+		mySelectedImage = new Image(SELECT_ARROW_URL);
 		mySelectedImageView = new ImageView(mySelectedImage);
-        initializeDisplay();
-        
-    }
+		initializeDisplay();
 
-    /**
-     * Update the drawable game element and its visual appearance.
-     */
-    @Override
-    public void update () {
-        super.update();
-        // state.update();
-        // Use polling because java.util.observable requires inheritance
-        // and javafx.beans.observable isn't serializable.
-        myAnimation.setAnimation(myState.getAnimation());
-        myAnimation.update();
-    }
+	}
 
-    /**
-     * Set the element's animation to a given sequence
-     * 
-     * @param animation the animation sequence to set
-     */
-    public void setAnimation (AnimationSequence animation) {
-        myAnimation.setAnimation(animation);
-    }
-    /**
-     * @return the current state of the element
-     */
-    public DrawableGameElementState getState () {
-        return myState;
-    }
-    
-    /**
-     * Gets the node that is being displayed on the scene
-     * 
-     * @return The game element
-     */
-    @Override
-    public Node getNode () {
-        return myDisplay;
-    }
-    
-    /**
-     * Gets the VBox of stats of the game element
-     * 
-     * @return The VBox of the game element
-     */
-    public VBox getDisplayVBox() {
-    	return myDisplayVBox;
-    }
-    
-    /**
-     * Get the bound array of the object
-     * @return the object's bounds
-     */
-    public double[] getBounds () {
-        return myState.getBounds();
-    }
+	/**
+	 * Update the drawable game element and its visual appearance.
+	 */
+	@Override
+	public void update() {
+		super.update();
+		// state.update();
+		// Use polling because java.util.observable requires inheritance
+		// and javafx.beans.observable isn't serializable.
+		myAnimation.setAnimation(myState.getAnimation());
+		myAnimation.update();
+	}
 
-    //TODO: Fix. Move logic into group
-    public void setLocation (Point2D location) {
-        myDisplay.setLayoutX(location.getX());
-        myState.setNumericalAttribute(DrawableGameElementState.X_POS_STRING, location.getX());
-        myDisplay.setLayoutY(location.getY());
-        myState.setNumericalAttribute(DrawableGameElementState.Y_POS_STRING, location.getY());
-    }
+	/**
+	 * Set the element's animation to a given sequence
+	 * 
+	 * @param animation
+	 *            the animation sequence to set
+	 */
+	public void setAnimation(AnimationSequence animation) {
+		myAnimation.setAnimation(animation);
+	}
 
-    public Point2D getLocation () {
-        Point2D p =
-                new Point2D(myAnimation.getNode().getLayoutX(), myAnimation.getNode().getLayoutY());
-        return p;
-    }
-    
-    /**
-     * Initializes the display for each game element
-     */
-    private void initializeDisplay() {
-    	 myDisplay.getChildren().add(myDisplayVBox);
-    	 myDisplayVBox.setAlignment(Pos.TOP_CENTER);
-    	 myDisplayVBox.getChildren().add(mySelectedImageView);
-    	 mySelectedImageView.setOpacity(0.0);
-    	 myDisplayVBox.getChildren().add(myAnimation.getNode());
-         myDisplay.setLayoutX(myState.getNumericalAttribute(DrawableGameElementState.X_POS_STRING)
-                 .doubleValue());
-         myDisplay.setLayoutY(myState.getNumericalAttribute(DrawableGameElementState.Y_POS_STRING)
-                 .doubleValue());
-         myDisplay.setTranslateX(-myAnimation.getDimension().getWidth() / 2);
-         myDisplay.setTranslateY(-myAnimation.getDimension().getHeight() / 2);
-    }
+	/**
+	 * @return the current state of the element
+	 */
+	public DrawableGameElementState getState() {
+		return myState;
+	}
+
+	/**
+	 * Gets the node that is being displayed on the scene
+	 * 
+	 * @return The game element
+	 */
+	@Override
+	public Node getNode() {
+		return myDisplay;
+	}
+
+	/**
+	 * Gets the VBox of stats of the game element
+	 * 
+	 * @return The VBox of the game element
+	 */
+	public VBox getDisplayVBox() {
+		return myDisplayVBox;
+	}
+
+	/**
+	 * Get the bound array of the object
+	 * 
+	 * @return the object's bounds
+	 */
+	public double[] getBounds() {
+		return myState.getBounds();
+	}
+
+	// TODO: Fix. Move logic into group
+	public void setLocation(Point2D location) {
+		myDisplay.setLayoutX(location.getX());
+		myState.setNumericalAttribute(DrawableGameElementState.X_POS_STRING,
+				location.getX());
+		myDisplay.setLayoutY(location.getY());
+		myState.setNumericalAttribute(DrawableGameElementState.Y_POS_STRING,
+				location.getY());
+	}
+
+	public Point2D getLocation() {
+		Point2D p = new Point2D(myAnimation.getNode().getLayoutX(), myAnimation
+				.getNode().getLayoutY());
+		return p;
+	}
+
+	/**
+	 * Initializes the display for each game element
+	 */
+	private void initializeDisplay() {
+		myDisplay.getChildren().add(myDisplayVBox);
+		myDisplayVBox.setAlignment(Pos.TOP_CENTER);
+		myDisplayVBox.getChildren().add(mySelectedImageView);
+		mySelectedImageView.setOpacity(0.0);
+		myDisplayVBox.getChildren().add(myAnimation.getNode());
+		myDisplay.setLayoutX(myState.getNumericalAttribute(
+				DrawableGameElementState.X_POS_STRING).doubleValue());
+		myDisplay.setLayoutY(myState.getNumericalAttribute(
+				DrawableGameElementState.Y_POS_STRING).doubleValue());
+		myDisplay.setTranslateX(-myAnimation.getDimension().getWidth() / 2);
+		myDisplay.setTranslateY(-myAnimation.getDimension().getHeight() / 2);
+	}
 }
--- src/game_engine/gameRepresentation/renderedRepresentation/SelectableGameElement.java
+++ src/game_engine/gameRepresentation/renderedRepresentation/SelectableGameElement.java
@@ -15,30 +15,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
-
-
 /**
- * A wrapper for game elements capable of being selected. Adds a "selected" visual appearance to the
- * appearance defined by the DrawableGameElement and handles animations for actions resulting from
- * being selected.
+ * A wrapper for game elements capable of being selected. Adds a "selected"
+ * visual appearance to the appearance defined by the DrawableGameElement and
+ * handles animations for actions resulting from being selected.
  *
  * @author Jonathan , Steve, Nishad, Rahul, John
  *
  */
 public class SelectableGameElement extends DrawableGameElement {
 
-	
 	private boolean isSelected;
-	//    private SelectableGameElementState myState;
+	// private SelectableGameElementState myState;
 	private Point2D heading;
-	//private boolean selected;
+	// private boolean selected;
 	// TODO temporary, should be in the attributes
 	private double speed = 3;
 
 	/**
 	 * @see DrawableGameElementState
 	 */
-	public SelectableGameElement (DrawableGameElementState element) {
+	public SelectableGameElement(DrawableGameElementState element) {
 		super(element);
 		this.isSelected = false;
 		// TODO Auto-generated constructor stub
@@ -47,7 +44,7 @@ public class SelectableGameElement extends DrawableGameElement {
 	/**
 	 * @return the type of the element
 	 */
-	public String getType () {
+	public String getType() {
 		return getState().getType();
 		// return myState.getType();
 	}
@@ -55,20 +52,21 @@ public class SelectableGameElement extends DrawableGameElement {
 	/**
 	 * Select the element
 	 *
-	 * @param select a boolean indicating whether the object should be selected
+	 * @param select
+	 *            a boolean indicating whether the object should be selected
 	 */
-	public void select (boolean select) {
+	public void select(boolean select) {
 		isSelected = select;
 		updateSelectedIndicator();
-		//myAnimation.select(select);
+		// myAnimation.select(select);
 	}
 
 	/**
-	 * Update the object due to internal influences then update the object due to collisions,
-	 * visible objects, and the current objective of the object
+	 * Update the object due to internal influences then update the object due
+	 * to collisions, visible objects, and the current objective of the object
 	 */
 	@Override
-	public void update () {
+	public void update() {
 		super.update();
 		updateSelfDueToCollisions();
 		updateSelfDueToVisions();
@@ -76,71 +74,76 @@ public class SelectableGameElement extends DrawableGameElement {
 		move();
 	}
 
-	private void move () {
-		if (heading == null) heading = getLocation();
+	private void move() {
+		if (heading == null)
+			heading = getLocation();
 		if (!heading.equals(getLocation())) {
-			Point2D delta =
-					new Point2D(heading.getX() - getLocation().getX(), heading.getY() -
-							getLocation().getY());
-			if (delta.magnitude() > speed) delta = delta.normalize().multiply(speed);
+			Point2D delta = new Point2D(heading.getX() - getLocation().getX(),
+					heading.getY() - getLocation().getY());
+			if (delta.magnitude() > speed)
+				delta = delta.normalize().multiply(speed);
 			this.setLocation(getLocation().add(delta));
 		}
 	}
 
-	private Point2D getLoc () {
-		return new Point2D(getState().getNumericalAttribute(DrawableGameElementState.X_POS_STRING)
-				.doubleValue(), getState()
-				.getNumericalAttribute(DrawableGameElementState.Y_POS_STRING)
-				.doubleValue());
+	private Point2D getLoc() {
+		return new Point2D(getState().getNumericalAttribute(
+				DrawableGameElementState.X_POS_STRING).doubleValue(),
+				getState().getNumericalAttribute(
+						DrawableGameElementState.Y_POS_STRING).doubleValue());
 	}
 
-	private void setLoc (Point2D location) {
-		getState().setNumericalAttribute(DrawableGameElementState.X_POS_STRING, location.getX());
-		getState().setNumericalAttribute(DrawableGameElementState.Y_POS_STRING, location.getY());
+	private void setLoc(Point2D location) {
+		getState().setNumericalAttribute(DrawableGameElementState.X_POS_STRING,
+				location.getX());
+		getState().setNumericalAttribute(DrawableGameElementState.Y_POS_STRING,
+				location.getY());
 
 	}
 
-	private void updateSelfDueToCurrentObjective () {
+	private void updateSelfDueToCurrentObjective() {
 		getApplicableConditionActionPairs("ObjectiveCondition");
 	}
 
-	public void updateSelfDueToSelection () {
+	public void updateSelfDueToSelection() {
 		getApplicableConditionActionPairs("SelfCondition");
 	}
 
-	private void updateSelfDueToVisions () {
-		evaluateConditionActionPairsOnInteractingElementsSubset("VisionCondition",
-				"VisibleElements");
+	private void updateSelfDueToVisions() {
+		evaluateConditionActionPairsOnInteractingElementsSubset(
+				"VisionCondition", "VisibleElements");
 	}
 
-	private void updateSelfDueToCollisions () {
-		evaluateConditionActionPairsOnInteractingElementsSubset("CollisionCondition",
-				"CollidingElements");
+	private void updateSelfDueToCollisions() {
+		evaluateConditionActionPairsOnInteractingElementsSubset(
+				"CollisionCondition", "CollidingElements");
 	}
 
 	private void updateSelectedIndicator() {
 		VBox myDisplayVBox = super.getDisplayVBox();
 		ImageView iv = (ImageView) myDisplayVBox.getChildren().get(0);
-		if(isSelected) {
+		if (isSelected) {
 			iv.setOpacity(1.0);
-		} 
-		else {
+		} else {
 			iv.setOpacity(0.0);
 		}
 	}
 
 	// TODO FIX THIS SHIT
-	private void evaluateConditionActionPairsOnInteractingElementsSubset (String conditionActionPairIdentifier,
-			String elementIdentifier) {
+	private void evaluateConditionActionPairsOnInteractingElementsSubset(
+			String conditionActionPairIdentifier, String elementIdentifier) {
 
 		// List<Entry<Evaluatable, Action>> applicableConditionActionPairs =
 		// getApplicableConditionActionPairs(conditionActionPairIdentifier);
-		// if (myState.getInteractingElements().containsKey(elementIdentifier)) {
-		// for (DrawableGameElementState element : myState.getInteractingElements()
+		// if (myState.getInteractingElements().containsKey(elementIdentifier))
+		// {
+		// for (DrawableGameElementState element :
+		// myState.getInteractingElements()
 		// .get(elementIdentifier)) {
 		// List<GameElementState> immediatelyInteractingElements =
 		// new ArrayList<GameElementState>();
-		// for (Entry<Evaluatable, Action> conditionActionPair : applicableConditionActionPairs) {
+		// for (Entry<Evaluatable, Action> conditionActionPair :
+		// applicableConditionActionPairs) {
 		// if (conditionActionPair.getKey().evaluate(this, element)) {
 		// conditionActionPair.getValue().doAction(immediatelyInteractingElements);
 		// }
@@ -150,18 +153,19 @@ public class SelectableGameElement extends DrawableGameElement {
 	}
 
 	// TODO FIX THIS SHIT
-	private List<Entry<Evaluatable, Action>> getApplicableConditionActionPairs (String conditionActionPairIdentifier) {
+	private List<Entry<Evaluatable, Action>> getApplicableConditionActionPairs(
+			String conditionActionPairIdentifier) {
 		// return this.myConditionActionPairs.entrySet().stream()
 
 		// .collect(Collectors.toList());
 		return null;
 	}
 
-	public boolean isSelected () {
+	public boolean isSelected() {
 		return this.isSelected;
 	}
 
-	public void setHeading (Point2D click) {
+	public void setHeading(Point2D click) {
 		this.heading = click;
 	}
 
