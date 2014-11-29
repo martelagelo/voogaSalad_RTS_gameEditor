18f5a075eee1fb1fc9d9cd1019415db67e42c027
Joshua Miller <jmm146@duke.edu>
2014-11-29 10:17:35 -0500

    add in multilange to wizards and main stage

--- src/editor/wizards/LevelWizard.java
+++ src/editor/wizards/LevelWizard.java
@@ -1,5 +1,7 @@
 package editor.wizards;
 
+import util.multilanguage.LanguageException;
+import util.multilanguage.MultiLanguageUtility;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
@@ -10,6 +12,9 @@ import javafx.scene.control.TextField;
  *
  */
 public class LevelWizard extends Wizard {
+	
+	private final String NEW_LEVEL_DEFAULT_KEY="NewLevelDefault";
+	private final String NEW_CAMPAIGN_DEFAULT_KEY="NewCampaignDefault";
     @FXML
     private TextField campaignName;
     @FXML
@@ -25,5 +30,18 @@ public class LevelWizard extends Wizard {
         setDataType(WizardDataType.LEVEL);
         addToData(WizardDataType.CAMPAIGN, campaignName.getText());
         addToData(WizardDataType.NAME, levelName.getText());
+    }
+    
+    @Override
+    protected void attachTextProperties (){
+    	MultiLanguageUtility util = MultiLanguageUtility.getInstance();
+		try{
+			levelName.textProperty().bind(util.getStringProperty(NEW_LEVEL_DEFAULT_KEY));
+			campaignName.textProperty().bind(util.getStringProperty(NEW_CAMPAIGN_DEFAULT_KEY));
+			super.attachTextProperties();
+		}
+		catch(LanguageException e){
+			//TODO Do something with this exception
+		}
     }
 }
