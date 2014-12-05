f0f51aaf416667e822e07238442aee6f856330e1
Nishad Agrawal <nishadagrawal7@gmail.com>
2014-12-05 17:38:04 -0500

    refactored grids into hierarchy

--- src/view/editor/wizards/AnimationGrid.java
+++ src/view/editor/wizards/AnimationGrid.java
@@ -1,10 +1,8 @@
 package view.editor.wizards;
 
 import java.util.ArrayList;
-import java.util.List;
 import javafx.scene.Group;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
 
 
 /**
@@ -13,25 +11,20 @@ import javafx.scene.shape.Rectangle;
  * @author Jonathan Tseng, Nishad Agrawal
  *
  */
-public class AnimationGrid extends Group {
-    private static final String FRAME_STROKE_COLOR = "black";
+public class AnimationGrid extends Grid {
     private static final String STOP_COLOR = "red";
     private static final String START_COLOR = "green";
     private static final String INVALID_STOP_MESSAGE = "Stop frame must come after start frame";
     private static final String INVALID_FRAME_MESSAGE = "this is an invalid frame number";
     private double myWidth;
     private double myHeight;
-    private double myFrameX;
-    private double myFrameY;
-    private List<List<Frame>> frameCols;
     private int start;
 
     public AnimationGrid (double imageWidth, double imageHeight, double frameX, double frameY) {
+        super(frameX, frameY);
         myWidth = imageWidth;
         myHeight = imageHeight;
-        frameCols = new ArrayList<>();
         changeSize(frameX, frameY);
-        start = 0;
     }
 
     public void changeSize (double frameX, double frameY) {
@@ -52,32 +45,25 @@ public class AnimationGrid extends Group {
     }
 
     private void selectFrame (int frameIndex, String color) throws IndexOutOfBoundsException {
-        if (frameIndex < 0 || frameIndex > (frameCols.size() * frameCols.get(0).size() - 1)) {
+        if (frameIndex < 0 || frameIndex > (frameCols.size() * frameCols.get(0).size() - 1)) { 
             throw new IndexOutOfBoundsException(INVALID_FRAME_MESSAGE); 
         }
-        resetExistingFill(color);
+        resetExistingFill(color, null);
         int col = frameIndex / frameCols.get(0).size();
         int row = frameIndex % frameCols.get(0).size();
+        selectFrame(row, col, Paint.valueOf(color));
+    }
+
+    @Override
+    public void selectFrame (int row, int col, Paint fill) {
         Frame f = frameCols.get(col).get(row);
         if (f.getFill() == null) {
-            f.setFill(Paint.valueOf(color));
+            f.setFill(fill);
         }
     }
 
-    private void resetExistingFill (String primary) {
-        frameCols.forEach(col -> col
-                .stream()
-                .filter(frame -> frame.getFill() != null &&
-                                 frame.getFill().equals(Paint.valueOf(primary)))
-                .forEach(frame -> frame.setFill(null)));
-    }
-
-    private void createGrid () {
-        frameCols.clear();
-        createColumns();
-    }
-
-    private void createColumns () {
+    @Override
+    protected void createColumns () {
         int col = 0;
         for (int i = 0; i < myWidth; i += myFrameX) {
             frameCols.add(col, new ArrayList<>());
@@ -88,11 +74,12 @@ public class AnimationGrid extends Group {
         }
     }
 
-    private Group createColumn (int col) {
+    @Override
+    protected Group createColumn (int col) {
         Group column = new Group();
         int row = 0;
         for (int i = 0; i < myHeight; i += myFrameY) {
-            Frame frame = createFrame(row, col);
+            Frame frame = createFrame(row, col, null);
             column.getChildren().add(frame);
             frame.setTranslateY(i);
             frameCols.get(col).add(row, frame);
@@ -101,32 +88,8 @@ public class AnimationGrid extends Group {
         return column;
     }
 
-    private Frame createFrame (int row, int col) {
-        Frame frame = new Frame(myFrameX, myFrameY);
-        frame.setStroke(Paint.valueOf(FRAME_STROKE_COLOR));
-        frame.setFill(null);
-        return frame;
-    }
-
-    public double getFrameX () {
-        return myFrameX;
-    }
-
-    public double getFrameY () {
-        return myFrameY;
-    }
-    
-    public int getNumColumns() {
+    public int getNumColumns () {
         return frameCols.size();
-    }
-
-    class Frame extends Rectangle {
-        public boolean selected;
-
-        public Frame (double x, double y) {
-            super(x, y);
-            selected = false;
-        }
     }
 
 }
