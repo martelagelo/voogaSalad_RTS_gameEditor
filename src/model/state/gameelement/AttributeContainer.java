d612524c4d4e21b43fab1f6de9a8e10b106f5ade
Michael Deng <mdeng123@gmail.com>
2014-12-05 21:47:57 -0500

    nothing

--- src/model/state/gameelement/AttributeContainer.java
+++ src/model/state/gameelement/AttributeContainer.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class AttributeContainer implements Serializable {
+public class AttributeContainer implements Serializable, Iterable {
     /**
      * 
      */
