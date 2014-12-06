a1dd150f1a24bff3ed7b0d11900dcc49f5ac8b79
Michael Deng <mdeng123@gmail.com>
2014-12-06 13:33:22 -0500

    modified type capabilities in GameElementState

--- src/engine/stateManaging/GameElementManager.java
+++ src/engine/stateManaging/GameElementManager.java
@@ -81,7 +81,6 @@ public class GameElementManager {
      * @param rectPoints
      *        the points in the rectangle surrounding the player's units
      */
-
     public void selectUnitsInBounds (double[] rectPoints, boolean multiSelect, Participant u) {
         boolean isTeamSelected = false;
         for (SelectableGameElement e : myLevel.getUnits()) {
