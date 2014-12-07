758acb37b655d9515b1031c28d6a6689f33ab6e4
lorejoh12 <jcl47@duke.edu>
2014-12-07 17:50:02 -0500

    making Participant JSONable

--- src/engine/users/EditorParticipant.java
+++ src/engine/users/EditorParticipant.java
@@ -2,6 +2,11 @@ package engine.users;
 
 public class EditorParticipant extends Participant {
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 3561060335779656127L;
+    
     public static final String EDITOR_NAME = "EDITOR";
 
     // TODO: color set to empty string for compilation
