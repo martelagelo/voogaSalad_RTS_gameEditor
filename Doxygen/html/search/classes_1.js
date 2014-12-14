d470dafb17f9eb196175be4438a968f23db8f474
Jonno Schmidt <jonathan.schmidt@duke.edu>
2014-12-14 04:20:14 +0000

    Added Readme

--- Doxygen/html/search/classes_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['badparameterformatexception',['BadParameterFormatException',['../d7/df4/classengine_1_1game_representation_1_1evaluatables_1_1parameters_1_1exceptions_1_1_bad_parameter_format_exception.html',1,'engine::gameRepresentation::evaluatables::parameters::exceptions']]],
-  ['bounce',['Bounce',['../d7/db1/classengine_1_1game_representation_1_1evaluatables_1_1evaluators_1_1_bounce.html',1,'engine::gameRepresentation::evaluatables::evaluators']]],
-  ['boundable',['Boundable',['../d6/def/interfacemodel_1_1state_1_1gameelement_1_1traits_1_1_boundable.html',1,'model::state::gameelement::traits']]],
-  ['boundswizard',['BoundsWizard',['../d0/d9a/classview_1_1editor_1_1wizards_1_1_bounds_wizard.html',1,'view::editor::wizards']]]
-];
