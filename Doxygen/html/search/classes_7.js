5c426230ab03cedd91181a2227fe26c23c43449f
Jonno Schmidt <jonathan.schmidt@duke.edu>
2014-12-14 08:51:11 +0000

    Added Readme

--- Doxygen/html/search/classes_7.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['headingupdate',['HeadingUpdate',['../de/d2e/classengine_1_1game_representation_1_1evaluatables_1_1evaluators_1_1_heading_update.html',1,'engine::gameRepresentation::evaluatables::evaluators']]],
-  ['highscore',['HighScore',['../d3/d8d/classmodel_1_1state_1_1_high_score.html',1,'model::state']]],
-  ['humanparticipant',['HumanParticipant',['../dc/d65/classengine_1_1users_1_1_human_participant.html',1,'engine::users']]]
-];
