8aad84f7daea0596ff2458de4c9c32c1a4801c2e
Jonno Schmidt <jonathan.schmidt@duke.edu>
2014-12-14 03:29:29 +0000

    Added Readme

--- Doxygen/html/search/files_5.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['falseevaluatable_2ejava',['FalseEvaluatable.java',['../de/dc3/_false_evaluatable_8java.html',1,'']]],
-  ['falseevaluator_2ejava',['FalseEvaluator.java',['../d8/dbf/_false_evaluator_8java.html',1,'']]],
-  ['follow_2ejava',['Follow.java',['../dd/d92/_follow_8java.html',1,'']]],
-  ['followx_2ejava',['FollowX.java',['../d6/df5/_follow_x_8java.html',1,'']]]
-];
--- Doxygen/html/search/files_d.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['objectconditionaction_2ejava',['ObjectConditionAction.java',['../d8/d3f/_object_condition_action_8java.html',1,'']]],
-  ['objectlocationcheckaction_2ejava',['ObjectLocationCheckAction.java',['../d7/d53/_object_location_check_action_8java.html',1,'']]],
-  ['objectofinterestidentifier_2ejava',['ObjectOfInterestIdentifier.java',['../df/d36/_object_of_interest_identifier_8java.html',1,'']]],
-  ['objecttypecheckaction_2ejava',['ObjectTypeCheckAction.java',['../d0/d5e/_object_type_check_action_8java.html',1,'']]],
-  ['or_2ejava',['Or.java',['../de/d4c/_or_8java.html',1,'']]]
-];
