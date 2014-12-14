48a76a23c30761be238d328d7d5c8dc27d0ef053
Jonno Schmidt <jonathan.schmidt@duke.edu>
2014-12-14 18:03:14 +0000

    Added Readme

--- Doxygen/html/search/classes_5.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['falseevaluatable',['FalseEvaluatable',['../d0/dd6/classengine_1_1game_representation_1_1evaluatables_1_1_false_evaluatable.html',1,'engine::gameRepresentation::evaluatables']]],
-  ['falseevaluator',['FalseEvaluator',['../da/d7e/classengine_1_1game_representation_1_1evaluatables_1_1evaluators_1_1_false_evaluator.html',1,'engine::gameRepresentation::evaluatables::evaluators']]],
-  ['follow',['Follow',['../d3/ddb/classengine_1_1game_representation_1_1evaluatables_1_1evaluators_1_1_follow.html',1,'engine::gameRepresentation::evaluatables::evaluators']]],
-  ['followx',['FollowX',['../da/d2c/classengine_1_1game_representation_1_1evaluatables_1_1evaluators_1_1_follow_x.html',1,'engine::gameRepresentation::evaluatables::evaluators']]],
-  ['frame',['Frame',['../d9/d78/classview_1_1editor_1_1wizards_1_1_grid_1_1_frame.html',1,'view::editor::wizards::Grid']]]
-];
